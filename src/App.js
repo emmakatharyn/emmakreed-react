460fd44875e0b6a4b7f5451221764e8bdab7ed01
Emma Reed <ereed@easitpa.com>
2023-11-15 23:26:05 -0700

    build responsive boostrap nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import "./App.css";
 
-import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+// import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import About from "./components/About";
@@ -11,7 +16,22 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
+      <Navbar className='header' collapseOnSelect expand='lg'>
+        <Container>
+          <Navbar.Brand className='navbar-brand m-4 ms-0' href='#home'>
+            EKR | DEV PORTFOLIO
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='gap-3 ms-auto'>
+              <Nav.Link href='#about'>About</Nav.Link>
+              <Nav.Link href='#projects'>Projects</Nav.Link>
+              <Nav.Link href='#contact'>Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <Main />
       <Footer />
     </>
