27297c376bb08f2c8ec3a01e30a7a070dabc234a
Emma Reed <ereed@easitpa.com>
2024-03-03 12:11:34 -0700

    hamburger icon; minor style adjustments

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { useDarkMode } from "../contexts/DarkModeContext";
 
 function Header() {
   const { darkMode, toggleDarkMode } = useDarkMode();
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const handleNavOpen = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  const handleNavLinkClick = () => {
+    setIsNavOpen(false);
+  };
+
   return (
-    <header className='header'>
+    <header className={isNavOpen ? "header nav-open" : "header"}>
       <Link className='header-link-home' to='home'>
         EKR | Dev Portfolio
       </Link>
@@ -19,26 +29,42 @@ function Header() {
             ></ion-icon>
           </button>
           <li>
-            <NavLink to='about' className='main-nav-link'>
+            <NavLink
+              to='about'
+              className='main-nav-link'
+              onClick={handleNavLinkClick}
+            >
               About
             </NavLink>
           </li>
           <li>
-            <NavLink to='projects' className='main-nav-link'>
+            <NavLink
+              to='projects'
+              className='main-nav-link'
+              onClick={handleNavLinkClick}
+            >
               Work
             </NavLink>
           </li>
           <li>
-            <NavLink to='contact' className='main-nav-link'>
+            <NavLink
+              to='contact'
+              className='main-nav-link'
+              onClick={handleNavLinkClick}
+            >
               Contact
             </NavLink>
           </li>
         </ul>
       </nav>
 
-      <button className='btn-mobile-nav'>
-        <ion-icon className='icon-mobile-nav' name='menu-outline'></ion-icon>
-        <ion-icon className='icon-mobile-nav' name='close-outline'></ion-icon>
+      <button className='btn-mobile-nav' onClick={handleNavOpen}>
+        <ion-icon
+          className='icon-mobile-nav'
+          name={isNavOpen ? "close-outline" : "menu-outline"}
+          style={{ height: "48px", width: "48px", color: "#ff008c" }}
+        ></ion-icon>
+        {/* <ion-icon className='icon-mobile-nav' name='close-outline'></ion-icon> */}
       </button>
     </header>
   );
